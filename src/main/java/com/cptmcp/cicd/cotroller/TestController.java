def8e44740b569d8c7095fd3c912473f224d8386
zt.melodic <zt.melodic@gmail.com>
2020-08-18 23:47:59 +0800

    maven release plugin test2

--- src/main/java/com/cptmcp/cicd/cotroller/TestController.java
+++ src/main/java/com/cptmcp/cicd/cotroller/TestController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/say")
     public String say(){
-        return "Hello";
+        return "Hello2";
     }
 }
